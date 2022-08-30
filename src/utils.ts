a06dc7e7528670b469e06019ece6ede410c91f9d
liaoliao666 <arthur.liao@huolala.cn>
2022-08-30 10:40:15 +0800

    fix: getKey

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import type { QueryKitKey, QueryKitPartialKey } from './types'
 
 export function genKeyFn<TVariables>(primaryKey: string) {
   return <V extends QueryKitPartialKey<TVariables>>(variables?: V) =>
-    (typeof variables === undefined
+    (typeof variables === 'undefined'
       ? [primaryKey]
       : [primaryKey, variables]) as QueryKitKey<V>
 }
