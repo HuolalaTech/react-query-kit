db7d6f243195c68c114983e4163f9c9a14dbf486
liaoxuan <arthur.liao@huolala.cn>
2024-03-21 15:56:36 +0800

    fix(react-query v4): remove keepPreviousData from suspense query

--- src/utils.ts
+++ src/utils.ts
@@ -10,7 +10,7 @@ export const isV5 = !!ReactQuery.useSuspenseQuery
 export const suspenseOptions = {
   enabled: true,
   suspense: true,
-  placeholderData: undefined,
+  keepPreviousData: undefined,
   useErrorBoundary: (_error: unknown, query: Query) =>
     query.state.data === undefined,
 }
